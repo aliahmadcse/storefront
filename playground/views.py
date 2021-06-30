935a72fb44f5b1376cb33fb6fc28b4c909c3b857
aliahmadcse <aliahmadcse@gmail.com>
2021-06-30 16:21:14 +0500

    📦 NEW: templates

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,8 @@
+from django.http.request import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
-def hello_world(request):
-    return HttpResponse("Hello World")
+def hello_world(request: HttpRequest):
+    return render(request, 'hello.html', {"name": "Ali Ahmad"})
